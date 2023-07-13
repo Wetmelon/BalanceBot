cbb108b86998599ae8015338ea261fdbe4271529
Paul Guenette <pfguenette@gmail.com>
2023-07-13 01:22:31 -0700

    Flesh out more behaviours

--- BalanceBot.ino
+++ BalanceBot.ino
@@ -23,7 +23,7 @@ PIControllerClass pitch_controller;
 PIControllerClass pitch_rate_controller;
 
 // Constants
-constexpr float kControlLoopPeriod = 1.0f;  // sec
+constexpr float kControlLoopPeriod = 0.01f;  // [sec]
 constexpr float kRadToDeg          = 360.0 / TWO_PI;
 constexpr float kPi                = PI;
 
@@ -31,7 +31,90 @@ constexpr float kWheelDiameter = 0.1524f;  // [m] Wheel Diameter
 constexpr float kTrackWidth    = 1.0f;     // [m] Distance between wheels
 constexpr float kJ             = 1.0f;     // [Nm/(rad/s^2)] Rotational inertia
 
-void readCAN(int packetSize) {
+struct CmdPair {
+    float drive;
+    float steer;
+};
+
+void setup() {
+    pinMode(LED_BUILTIN, OUTPUT);
+    pinMode(PIN_CAN_STANDBY, OUTPUT);
+    pinMode(PIN_CAN_BOOSTEN, OUTPUT);
+
+    // Enable CAN transceiver
+    digitalWrite(PIN_CAN_STANDBY, false);  // turn off STANDBY
+    digitalWrite(PIN_CAN_BOOSTEN, true);   // turn on booster
+
+    Serial.begin(115200);
+    Serial.println(F("Connected to serial at 115200 baud."));
+
+    // Start CAN at 250kbps
+    CAN.begin(250000);
+    CAN.onReceive(onCanRx);
+
+    Serial.println(F("Connected to CAN at 250kbps"));
+
+    // Enable i2c at 400kHz
+    Wire.begin();
+    Wire.setClock(400000);
+
+    // Request the IMU send the rotation vector and IMU at 100Hz
+    imu.begin();
+    imu.enableRotationVector((uint16_t)(kControlLoopPeriod * 1000.0f));
+    imu.enableGyro((uint16_t)(kControlLoopPeriod * 1000.0f));
+}
+
+void loop() {
+    static uint32_t last_run = millis();
+
+    // Blink the LED at 1Hz
+    blink(1000);
+
+    // We run the control loop when there's data available from the IMU (100Hz)
+    const uint32_t now = millis();
+    if ((now - last_run) >= (uint32_t)(kControlLoopPeriod * 1000.0f)) {
+        const CmdPair rx     = getControllerCmds();
+        const CmdPair rx_lpf = filterCmds(rx);
+
+        const float drive_torque = driveControl(rx_lpf.drive);
+        const float steer_torque = steerControl(rx_lpf.steer);
+
+        right_motor.set_input_torque_msg.Input_Torque = (drive_torque + steer_torque) * 0.5f;
+        left_motor.set_input_torque_msg.Input_Torque  = (drive_torque - steer_torque) * 0.5f;
+
+        sendCan();
+    }
+}
+
+float driveControl(const float vel_target) {
+    // Pitch angle and rate from IMU
+    const float pitch_angle = imu.getPitch();  // [rad] Pitch angle in world frame
+    const float pitch_rate  = imu.getGyroX();  // [rad/s] Rate of rotation about pitch axis in IMU frame
+
+    // Motor speeds
+    const float vel_right = right_motor.get_encoder_estimates_msg.Vel_Estimate * (kWheelDiameter * kPi);  // [m/s] right wheel speed
+    const float vel_left  = left_motor.get_encoder_estimates_msg.Vel_Estimate * (kWheelDiameter * kPi);   // [m/s] left wheel speed
+
+    // TODO:  Verify yaw rate calculation matches gyro reading
+    const float vel_actual = (vel_right + vel_left) / 2.0f - pitch_rate;     // [m/s] Vehicle speed
+    const float yaw_rate   = (vel_right - vel_left) / (2.0f * kTrackWidth);  // [rad/s] Vehicle yaw Rate, per motor sensors
+    const float gyro_yaw   = imu.getGyroZ();                                 // [rad/s] Vehicle yaw rate, per gyro
+
+    // Run P/PI/P control loop
+    const float pitch_cmd      = vel_controller.update(true, vel_target, vel_actual, kControlLoopPeriod);
+    const float pitch_rate_cmd = pitch_controller.update(true, pitch_cmd, pitch_angle, kControlLoopPeriod);
+
+    // Pitch rate controller outputs an acceleration, and Tau = J * alpha
+    const float torque_cmd = pitch_rate_controller.update(true, pitch_rate_cmd, pitch_rate, kControlLoopPeriod) * kJ;
+
+    return torque_cmd;
+}
+
+float steerControl(const float yaw_target) {
+    return 0.0f;
+}
+
+void onCanRx(int packetSize) {
     can_Message_t rxmsg;
     rxmsg.id = CAN.packetId();
     CAN.readBytes(rxmsg.data, 8);
@@ -47,84 +130,35 @@ void sendCan() {
     const can_Message_t left_torque_msg  = left_motor.encode(ODriveCAN::kSetInputTorqueMsg);
     const can_Message_t right_torque_msg = right_motor.encode(ODriveCAN::kSetInputTorqueMsg);
 
-    // Adafruit Feather
     CAN.write(left_torque_msg.data, 8);
     CAN.write(right_torque_msg.data, 8);
 }
 
-void setup() {
-    pinMode(LED_BUILTIN, OUTPUT);
-    pinMode(PIN_CAN_STANDBY, OUTPUT);
-    pinMode(PIN_CAN_BOOSTEN, OUTPUT);
+void blink(const uint32_t blink_period_ms) {
+    static uint32_t last_blink = millis();
+    static bool led_state      = false;
 
-    // Enable CAN transceiver
-    digitalWrite(PIN_CAN_STANDBY, false);  // turn off STANDBY
-    digitalWrite(PIN_CAN_BOOSTEN, true);   // turn on booster
+    const uint32_t now = millis();
+    if ((now - last_blink) >= (blink_period_ms / 2UL)) {
+        last_blink = now;
+        led_state  = !led_state;
 
-    Serial.begin(115200);
-    Serial.println(F("Connected to serial at 115200 baud."));
-
-    if (!CAN.begin(250000L)) {
-        Serial.println(F("CAN.begin(...) failed."));
-        // Flash LED?
-        for (;;) {
-            delay(10);
-        }
+        digitalWrite(LED_BUILTIN, led_state ? HIGH : LOW);
     }
-
-    CAN.onReceive(readCAN);
-
-    Serial.println(F("Connected to CAN at 250kbps"));
-
-    Wire.begin();  // Enable i2c
-    imu.begin();
-
-    Wire.setClock(400000);  // Increase i2c data rate to 400khz
-
-    // // 100hz reporting
-    // imu.enableRotationVector(10);
-    // imu.enableGyro(10);
 }
 
-void loop() {
-    static bool led_state    = LOW;
-    static uint32_t last_run = micros();
-    if ((micros() - last_run) > (uint32_t)(kControlLoopPeriod * 1e6f)) {
-        last_run = micros();
+CmdPair filterCmds(const CmdPair& rx) {
+    // Lowpass filters
+    static odrv::lpf drive_lpf{kControlLoopPeriod, 0.5f};
+    static odrv::lpf steer_lpf{kControlLoopPeriod, 1.0f};
 
-        led_state = !led_state;
-        digitalWrite(LED_BUILTIN, led_state ? HIGH : LOW);
-        Serial.print("vbus: ");
-        Serial.println(left_motor.get_bus_voltage_current_msg.Bus_Voltage);
+    return {
+        drive_lpf.step(rx.drive),
+        steer_lpf.step(rx.steer),
+    };
+}
 
-        const float vel_target = 0.0f;  // Get velocity target from somewhere
-        const float yaw_target = 0.0f;  // Get yaw rate target from somewhere
-
-        // Only bother to update the controller when we have new data available from the IMU
-
-        // Pitch angle and rate from IMU
-        const float pitch_angle = imu.getPitch();  // [rad] Pitch angle
-        const float pitch_rate  = imu.getGyroX();  // [rad/s] Rate of rotation about pitch axis
-
-        // Motor speeds
-        const float vel_right = right_motor.get_encoder_estimates_msg.Vel_Estimate * (kWheelDiameter * kPi);  // [m/s] right wheel speed
-        const float vel_left  = left_motor.get_encoder_estimates_msg.Vel_Estimate * (kWheelDiameter * kPi);   // [m/s] left wheel speed
-
-        // TODO:  Verify yaw rate calculation matches gyro reading
-        const float vel_actual = (vel_right + vel_left) / 2.0f;                  // [m/s] Vehicle speed
-        const float yaw_rate   = (vel_right - vel_left) / (2.0f * kTrackWidth);  // [rad/s] Vehicle Yaw Rate, per motor sensors
-        const float gyro_yaw   = imu.getGyroZ();                                 // [rad/s] Vehicle yaw rate, per gyro
-
-        // Run P/PI/P control loop
-        const float pitch_cmd      = vel_controller.update(true, vel_target, vel_actual, kControlLoopPeriod);
-        const float pitch_rate_cmd = pitch_controller.update(true, pitch_cmd, pitch_angle, kControlLoopPeriod);
-
-        // Pitch rate controller outputs an acceleration, and Tau = J * alpha
-        const float torque_cmd = pitch_rate_controller.update(true, pitch_rate_cmd, pitch_rate, kControlLoopPeriod) * kJ;
-
-        left_motor.set_input_torque_msg.Input_Torque  = torque_cmd * 0.5f;
-        right_motor.set_input_torque_msg.Input_Torque = torque_cmd * 0.5f;
-
-        sendCan();
-    }
+CmdPair getControllerCmds() {
+    // Read RC controller
+    return {};
 }
--- utils.hpp
+++ utils.hpp
@@ -7,4 +7,17 @@ template <class T>
 constexpr const T& clamp(const T& x, const T& lo, const T& hi) {
     return std::min(std::max(x, lo), hi);
 }
+
+struct lpf {
+    lpf(float Ts, float Tau) : _alpha(Ts / (Ts + Tau)) {}
+
+    float step(float signal) {
+        _state += _alpha * (signal - _state);
+        return _state;
+    }
+
+   private:
+    float _state       = 0.0f;
+    const float _alpha = 0.0f;
+};
 }  // namespace odrv
